353a3c1cf571ca7a23e4c6ecc0981f14cc2d0455
Geet-04 <geetanjalisahu428@gmail.com>
2025-07-08 18:15:12 +0530

    Updated files after editing

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -73,7 +73,7 @@ const About = () => {
           transitionSpeed={1000}
           gyroscope={true}
           >
-          <img src={profileImage} alt="Geetanjali Sahu"
+          <img src={import.meta.env.VITE_PROFILE_IMAGE1} alt="Geetanjali Sahu"
           className='w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,.5)]'
           />
         </Tilt>
@@ -82,5 +82,6 @@ const About = () => {
     </section>
   )
 }
+
 
 export default About
