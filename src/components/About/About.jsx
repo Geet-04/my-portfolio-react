1406a316b8135e50808545604dafd2853ac438d3
Geet-04 <geetanjalisahu428@gmail.com>
2025-07-09 11:11:53 +0530

    updated About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -50,7 +50,7 @@ const About = () => {
         </p>
         {/* Resume Button */}
         <a
-        href="https://drive.google.com/file/d/1JsZZ4TOQDznV1Hm-FeamaBxEtHAxHebY/view?usp=sharing"
+        href="https://docs.google.com/document/d/1W7LGhGOXamQ_0CfkEDA1tzXhzOl_8Lbn/edit?usp=sharing&ouid=100597983848924858047&rtpof=true&sd=true"
         target = "_blank"
         rel='noopener noreferrer'
         className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
